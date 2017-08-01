6f26a0b11f8d33c1139fbc4fa44f9091a7e4f419
diggerdata <robotincad@gmail.com>
2017-08-01 12:39:14 -0400

    Moved HID object to class

--- src/hid.m
+++ src/hid.m
@@ -6,40 +6,25 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.lang.*;
 
-hidServices = HidManager.getHidServices();
-hidServices.start();
-hidDevices = hidServices.getAttachedHidDevices();
-dev=hidDevices.toArray;
 
-for i=1:(dev.length)
-    if dev(i).getProductId() == 7
-        dev(i).open();
-%         nucleo = dev(i);
-        if dev(i).isOpen()
-            pp = PacketProcessor(dev(i));
+pp = PacketProcessor(7);
 
-            sum = 0;
-            min = 100000;
-            max = 0;
-            values = zeros(15, 1, 'single');
-            sinWaveInc = 2000;
-            seconds = 0.01;
-            range = 400;
+sum = 0;
+min = 100000;
+max = 0;
+values = zeros(15, 1, 'single');
+sinWaveInc = 2000;
+seconds = 0.01;
+range = 400;
 
-    %         for k=1:size(sinWaveInc)
-    %             for j=1:4
-    %                 values((j * 3) + 0) = (sin((k / sinWaveInc * pi *2) * range) -range);
-    %                 values((j * 3) + 1) = 0;
-    %                 values((j * 3) + 2) = 3;
-    %             end
-                returnValues = pp.command(37, values)   
-    %             timeit(returnValues)
-    %         end
-        end
-    end
-end
+%         for k=1:size(sinWaveInc)
+%             for j=1:4
+%                 values((j * 3) + 0) = (sin((k / sinWaveInc * pi *2) * range) -range);
+%                 values((j * 3) + 1) = 0;
+%                 values((j * 3) + 2) = 3;
+%             end
+returnValues = pp.command(37, values)
+%             timeit(returnValues)
+%         end
 
-dev(i).close()
-hidServices.shutdown();
- 
-% clear java;
+clear java;
