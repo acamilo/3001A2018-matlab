662b33aca508b6a87c532fede95b2e8cf0a66e70
Kevin Harrington <mad.hephaestus@gmail.com>
2017-08-27 17:49:49 -0400

    remove dh params

--- src/hid.m
+++ src/hid.m
@@ -37,50 +37,50 @@ range = 400.0;
 pp.shutdown()
 clear java;
 %Load the xml file
-xDoc = xmlread('seaArm.xml');
-%All Arms
-allAppendage =xDoc.getElementsByTagName('appendage');
-%All walking legs
-%allAppendage =xDoc.getElementsByTagName('leg');
-%All drivabel wheels
-%allAppendage =xDoc.getElementsByTagName('drivable');
-%All steerable wheels
-%allAppendage =xDoc.getElementsByTagName('steerable');
-%Grab the first appendage
-appendages = allAppendage.item(0);
-%all the D-H parameter tags
-allListitems = appendages.getElementsByTagName('DHParameters');
-%Load the transfrom of home to the base of the arm
-baseTransform = appendages.getElementsByTagName('baseToZframe').item(0);
-%Print all the values
-printTag(baseTransform,'x');
-printTag(baseTransform,'y');
-printTag(baseTransform,'z');
-printTag(baseTransform,'rotw');
-printTag(baseTransform,'rotx');
-printTag(baseTransform,'roty');
-printTag(baseTransform,'rotz');
-% Print D-H parameters
-for k = 0:allListitems.getLength-1
-   thisListitem = allListitems.item(k);
-   fprintf('\nLink %i\n',k);
-   % Get the label element. In this file, each
-   % listitem contains only one label.
-   printTag(thisListitem,'Delta');
-   printTag(thisListitem,'Theta');
-   printTag(thisListitem,'Radius');
-   printTag(thisListitem,'Alpha');
-end
-% Get the value stored in a tag
-function value = tagValue(thisListitem,name)
-   % listitem contains only one label.
-   thisList = thisListitem.getElementsByTagName(name);
-   thisElement = thisList.item(0);
-   data  = thisElement.getFirstChild.getData;
-   value = str2double(data);
-end
-%Print out the tag name with its value
-function printTag(thisListitem,name)
-   data  = tagValue(thisListitem,name);
-   fprintf('%s \t%f\n',name,data);
-end
+% xDoc = xmlread('seaArm.xml');
+% %All Arms
+% allAppendage =xDoc.getElementsByTagName('appendage');
+% %All walking legs
+% %allAppendage =xDoc.getElementsByTagName('leg');
+% %All drivabel wheels
+% %allAppendage =xDoc.getElementsByTagName('drivable');
+% %All steerable wheels
+% %allAppendage =xDoc.getElementsByTagName('steerable');
+% %Grab the first appendage
+% appendages = allAppendage.item(0);
+% %all the D-H parameter tags
+% allListitems = appendages.getElementsByTagName('DHParameters');
+% %Load the transfrom of home to the base of the arm
+% baseTransform = appendages.getElementsByTagName('baseToZframe').item(0);
+% %Print all the values
+% printTag(baseTransform,'x');
+% printTag(baseTransform,'y');
+% printTag(baseTransform,'z');
+% printTag(baseTransform,'rotw');
+% printTag(baseTransform,'rotx');
+% printTag(baseTransform,'roty');
+% printTag(baseTransform,'rotz');
+% % Print D-H parameters
+% for k = 0:allListitems.getLength-1
+%    thisListitem = allListitems.item(k);
+%    fprintf('\nLink %i\n',k);
+%    % Get the label element. In this file, each
+%    % listitem contains only one label.
+%    printTag(thisListitem,'Delta');
+%    printTag(thisListitem,'Theta');
+%    printTag(thisListitem,'Radius');
+%    printTag(thisListitem,'Alpha');
+% end
+% % Get the value stored in a tag
+% function value = tagValue(thisListitem,name)
+%    % listitem contains only one label.
+%    thisList = thisListitem.getElementsByTagName(name);
+%    thisElement = thisList.item(0);
+%    data  = thisElement.getFirstChild.getData;
+%    value = str2double(data);
+% end
+% %Print out the tag name with its value
+% function printTag(thisListitem,name)
+%    data  = tagValue(thisListitem,name);
+%    fprintf('%s \t%f\n',name,data);
+% end
