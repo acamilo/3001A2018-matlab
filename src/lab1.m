2bd51caa8255ab02a3cf79e5ca5b5704443ad1e7
Kevin Harrington <mad.hephaestus@gmail.com>
2018-01-23 09:03:59 -0500

    Place try catch around all code dealing with the HID device
    Once the HID device is instantiated, all code must happen in a try catch. After the catch, the shutdown command must be called. This ensures a clean shutdown of the HID hardware.

--- src/lab1.m
+++ src/lab1.m
@@ -22,44 +22,47 @@ import java.lang.*;
 
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = PacketProcessor(7); % !FIXME why is the deviceID == 7?
-SERV_ID = 37;            % we will be talking to server ID 37 on
-                         % the Nucleo
+try
+  SERV_ID = 37;            % we will be talking to server ID 37 on
+                           % the Nucleo
 
-DEBUG   = true;          % enables/disables debug prints
+  DEBUG   = true;          % enables/disables debug prints
 
-% Instantiate a packet - the following instruction allocates 64
-% bytes for this purpose. Recall that the HID interface supports
-% packet sizes up to 64 bytes.
-packet = zeros(15, 1, 'single');
+  % Instantiate a packet - the following instruction allocates 64
+  % bytes for this purpose. Recall that the HID interface supports
+  % packet sizes up to 64 bytes.
+  packet = zeros(15, 1, 'single');
 
-% The following code generates a sinusoidal trajectory to be
-% executed on joint 1 of the arm and iteratively sends the list of
-% setpoints to the Nucleo firmware. 
-viaPts = [0, -400, 400, -400, 400, 0];
+  % The following code generates a sinusoidal trajectory to be
+  % executed on joint 1 of the arm and iteratively sends the list of
+  % setpoints to the Nucleo firmware. 
+  viaPts = [0, -400, 400, -400, 400, 0];
 
-tic
+  tic
 
-% Iterate through a sine wave for joint values
-for k = viaPts
-    %incremtal = (single(k) / sinWaveInc);
-    
-    packet(1) = k;
-    
-    
-    % Send packet to the server and get the response
-    returnPacket = pp.command(SERV_ID, packet);
-    toc
-    
-    if DEBUG
-        disp('Sent Packet:');
-        disp(packet);
-        disp('Received Packet:');
-        disp(returnPacket);
-    end
-    
-    pause(1) %timeit(returnPacket) !FIXME why is this needed?
+  % Iterate through a sine wave for joint values
+  for k = viaPts
+      %incremtal = (single(k) / sinWaveInc);
+
+      packet(1) = k;
+
+
+      % Send packet to the server and get the response
+      returnPacket = pp.command(SERV_ID, packet);
+      toc
+
+      if DEBUG
+          disp('Sent Packet:');
+          disp(packet);
+          disp('Received Packet:');
+          disp(returnPacket);
+      end
+
+      pause(1) %timeit(returnPacket) !FIXME why is this needed?
+  end
+catch
+    disp('Exited on error, clean shutdown');
 end
-
 % Clear up memory upon termination
 pp.shutdown()
 clear java;
